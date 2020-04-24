0930cb37d785dc7d21f2e21c85fc7dfb0217f1b2
abap34 <uchi_ymgc@her0.jp>
2020-04-24 18:57:45 +0900

    Update __init__.py

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,12 +1,10 @@
-from core_simple import Variable
-from core_simple import Function
-from core_simple import using_config
-from core_simple import no_grad
-from core_simple import as_array
-from core_simple import as_variable
-from core_simple import as_tuple
-from core_simple import setup_variable
+from dezero.core import Variable
+from dezero.core import Function
+from dezero.core import using_config
+from dezero.core import no_grad
+from dezero.core import as_array
+from dezero.core import as_variable
+from dezero.core import as_tuple
+from dezero.core import setup_variable
 
 setup_variable()
-
-
