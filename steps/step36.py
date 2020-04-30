f598d71a3f03e72cb79c61deaa69332fba5007a1
abap34 <uchi_ymgc@her0.jp>
2020-04-30 21:39:02 +0900

    Create step36.py

---
+++ steps/step36.py
@@ -0,0 +1,21 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+
+x = Variable(np.array(2.0))
+y = x ** 2
+y.backward(create_graph=True)
+gx = x.grad
+print('gx:',gx)
+x.clearglad()
+print('gx2:',gx)
+
+z = gx ** 3 + y
+print('z:',z)
+z.backward()
+print('gx3:',gx)
+print('y:',y)
+print(x.grad)
