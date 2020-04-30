c03dfbcba0ea80bdebcc268d9b71a606573be16e
abap34 <uchi_ymgc@her0.jp>
2020-04-30 23:34:02 +0900

    Update utils.py

--- dezero/utils.py
+++ dezero/utils.py
@@ -68,3 +68,42 @@ def plot_dot_graph(output, verbose=True, to_file='graph.png'):
     extension = os.path.splitext(to_file)[1][1:]
     cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
     subprocess.run(cmd, shell=True)
+
+
+
+
+
+def sum_to(x, shape):
+    ndim = len(shape)
+    lead = x.ndim - ndim
+    lead_axis = tuple(range(lead))
+
+    axis = tuple([i + lead for i, sx in enumerate(shape) if sx == 1])
+    y = x.sum(lead_axis + axis, keepdims=True)
+    if lead > 0:
+        y = y.squeeze(lead_axis)
+    return y
+
+
+
+
+
+def reshape_sum_backward(gy, x_shape, axis, keepdims):
+    ndim = len(x_shape)
+    tupled_axis = axis
+
+    if axis is None:
+        tupled_axis = None
+    elif not hasattr(axis, 'len'):
+        tupled_axis = (axis,)
+
+    if not (ndim == 0 or tupled_axis is None or keepdims):
+        actual_axis = [a if a >= 0 else a + ndim for a in tupled_axis]
+        shape = list(gy.shape)
+        for a in sorted(actual_axis):
+            shape.insert(a,1)
+    else:
+        shape = gy.shape
+    
+    gy = gy.reshape(shape)
+    return gy
