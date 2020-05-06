8713c15abaaf20a72c424653b1538f59aafe6b4b
abap34 <uchi_ymgc@her0.jp>
2020-05-06 23:34:47 +0900

    Create step43.py

---
+++ steps/step43.py
@@ -0,0 +1,62 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dezero.core import Variable
+import dezero.functions as F
+from dezero.utils import plot_dot_graph
+
+np.random.seed(0)
+x = np.random.rand(100,1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100,1)
+
+I,H,O = 1, 10, 1
+W1 = Variable(0.01 * np.random.randn(I,H))
+b1 = Variable(np.zeros(H))
+W2 = Variable(0.01 * np.random.randn(H,O))
+b2 = Variable(np.zeros(O))
+
+
+def predict(x):
+    y = F.linear(x,W1,b1)
+    y = F.sigmoid_simple(y)
+    y = F.linear(y,W2,b2)
+    return y
+
+
+lr = 0.2
+iters = 10000
+
+
+for i in range(iters):
+    y_pred = predict(y)
+    loss = F.mean_squared_error(y, y_pred)
+
+    W1.cleargrad()
+    b1.cleargrad()
+    W2.cleargrad()
+    b2.cleargrad()
+    loss.backward()
+
+    W1.data -= lr * W1.grad.data
+    b1.data -= lr * b1.grad.data
+    W2.data -= lr * W2.grad.data
+    b2.data -= lr * b2.grad.data
+
+    if i % 1000 == 0:
+        print(loss)
+
+
+
+
+plt.scatter(x, y, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+test = np.arange(0,1,0.01)[:,np.newaxis]
+y_pred = predict(test)
+
+plt.plot(test, y_pred.data, color='r')
+plt.show()
