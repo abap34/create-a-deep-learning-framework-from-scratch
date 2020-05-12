d7459648282ef90df36d07fe0955df3c0ab9481e
abap34 <uchi_ymgc@her0.jp>
2020-05-12 20:04:18 +0900

    Update layers.py

--- dezero/layers.py
+++ dezero/layers.py
@@ -23,7 +23,11 @@ class Layer:
         raise NotImplementedError()
     def params(self):
         for name in self._params:
-            yield self.__dict__[name]
+            obj = self.__dict__[name]
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
     def cleargrads(self):
         for param in self.params():
             param.cleargrad()
