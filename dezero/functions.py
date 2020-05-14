b8c420768f46f8dd4cd46473f580525bd3276b59
abap34 <uchi_ymgc@her0.jp>
2020-05-14 18:33:11 +0900

    Update functions.py

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,6 +1,8 @@
 import numpy as np
+from dezero.core import Variable
 from dezero.core import Function
 from dezero.core import as_variable
+from dezero.core import as_array
 from dezero import utils
 
 
@@ -280,3 +282,10 @@ def get_item(x, slices):
     return f(x)
 
 
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
