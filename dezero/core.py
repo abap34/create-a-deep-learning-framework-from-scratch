31eca561d582045d21f01670029a87fa5030e3b0
abap34 <uchi_ymgc@her0.jp>
2020-05-15 18:09:44 +0900

    Update core.py; add model's mode; train/test

--- dezero/core.py
+++ dezero/core.py
@@ -244,7 +244,7 @@ def pow(x,c):
 
 class Config:
     enable_backprop = True
-
+    train = True
 
 
 @contextlib.contextmanager
@@ -258,7 +258,10 @@ def using_config(name,value):
 
 
 def no_grad():
-    return using_config('enable_backprop',False)
+    return using_config('enable_backprop', False)
+
+def test_mode():
+    return using_config('train', False)
 
 
 
