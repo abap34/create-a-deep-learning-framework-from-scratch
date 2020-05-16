18a08194593596f616976fb80b11190d89bc2bec
abap34 <uchi_ymgc@her0.jp>
2020-05-16 15:42:05 +0900

    Update layers.py; create Conv2d

--- dezero/layers.py
+++ dezero/layers.py
@@ -6,7 +6,7 @@ from dezero.core import Paramater
 from dezero import as_tuple
 from dezero import cuda
 import dezero.functions as F
-
+from dezero.utils import pair
 
 class Layer:
     def __init__(self):
@@ -109,3 +109,41 @@ class Linear(Layer):
         y = F.linear(x, self.W, self.b)
         return y
 
+
+class Conv2d(Layer):
+    def __init__(self, out_channels, kernel_size, stride=1, pad=0, nobias=False, dtype=np.float32, in_channels=None):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+
+        self.W = Paramater(None, name='W')
+
+        if in_channels is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Paramater(np.zeros(out_channels, dtype=dtype), name='b')
+
+        def _init_W(self, xp=np):
+            C, OC = self.in_channels, self.out_channels
+            KH, KW = pair(self.kernel_size)
+            scale = np.sqrt(1 / (C * KH * KW))
+            W_data = xp.random.randn(OC, C, KH, KW).astype(self.dtype) * scale
+            self.W.data = W_data
+        
+        def forward(self, x):
+            if self.W.data is None:
+                self.in_channels_x.shape[1]
+                xp = cuda.get_array_module(x)
+                self._init_W(xp)
+
+            y = F.conv2d_simple(x, self.W, self.b, self.stride, self.pad)
+            return y
+
+        
