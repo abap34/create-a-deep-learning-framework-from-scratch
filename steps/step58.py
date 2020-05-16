03b4f77224115675fa0be0116c2b79b624cc6fc0
abap34 <uchi_ymgc@her0.jp>
2020-05-16 18:50:04 +0900

    Update step58.py; test VGG16

--- steps/step58.py
+++ steps/step58.py
@@ -5,9 +5,18 @@ if '__file__' in globals():
 
 
 import numpy as np
+from PIL import Image
+import dezero
 from dezero.models import VGG16
 
+url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
+img_path = dezero.utils.get_file(url)
+img = Image.open(img_path)
+x = VGG16.preprocess(img)
+x = x[np.newaxis]
 model = VGG16(pretrained=True)
-
-x = np.random.rand(1, 3, 224, 224)
-model.plot(x, to_file='VGG16.png')
+with dezero.test_mode():
+    y = model(x)
+predict_id = np.argmax(y.data)
+labels = dezero.datasets.ImageNet.labels()
+print(labels[predict_id])
