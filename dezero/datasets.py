26bf0f1c5b8a5ea80ead17cf0889a46f968a27e1
abap34 <uchi_ymgc@her0.jp>
2020-05-16 18:51:15 +0900

    Update datasets.py; add ImageNet

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -1,4 +1,5 @@
 import numpy as np
+from dezero.utils import get_file
 
 class Dataset:
     def __init__(self, train=True, transform=lambda x: x, target_transform=lambda x: x):
@@ -58,3 +59,16 @@ class Spiral(Dataset):
     def prepare(self):
         self.data, self.label = get_spiral(self.train)
 
+
+class ImageNet(Dataset):
+
+    def __init__(self):
+        NotImplemented
+
+    @staticmethod
+    def labels():
+        url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
+        path = get_file(url)
+        with open(path, 'r') as f:
+            labels = eval(f.read())
+        return labels
