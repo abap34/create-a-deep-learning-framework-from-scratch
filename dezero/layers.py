6855f66a437b31a7bfd4cbefa839a705572fde37
abap34 <uchi_ymgc@her0.jp>
2020-05-18 20:39:20 +0900

    Update layers.py; add LSTM

--- dezero/layers.py
+++ dezero/layers.py
@@ -162,3 +162,45 @@ class RNN(Layer):
             h_new = F.tanh(self.x2h(x) + self.h2h(self.h))
         self.h = h_new
         return h_new
+
+
+class LSTM(Layer):
+    def __init__(self, hidden_size, in_size=None):
+        super().__init__()
+
+        H, I = hidden_size, in_size
+        self.x2f = Linear(H, in_size=I)
+        self.x2i = Linear(H, in_size=I)
+        self.x2o = Linear(H, in_size=I)
+        self.x2u = Linear(H, in_size=I)
+        self.h2f = Linear(H, in_size=H, nobias=True)
+        self.h2i = Linear(H, in_size=H, nobias=True)
+        self.h2o = Linear(H, in_size=H, nobias=True)
+        self.h2u = Linear(H, in_size=H, nobias=True)
+        self.reset_state()
+
+    def reset_state(self):
+        self.h = None    
+        self.c = None        #cell-state
+    
+    def forward(self, x):
+        if self.h is None:   #最初
+            f = F.sigmoid(self.x2f(x))  # forget gate
+            i = F.sigmoid(self.x2i(x))  # input gate
+            o = F.sigmoid(self.x2o(x))  # output gate
+            u = F.tanh(self.x2u(x))     
+        else:
+            f = F.sigmoid(self.x2f(x) + self.h2f(self.h))
+            i = F.sigmoid(self.x2i(x) + self.h2i(self.h))
+            o = F.sigmoid(self.x2o(x) + self.h2o(self.h))
+            u = F.tanh(self.x2u(x) + self.h2u(self.h))
+        if self.c is None:
+            c_new = (i * u)           # input gateに通す
+        else:
+            c_new = (f * self.c) + (i * u)  # forget gateとinput gateに通す
+        
+        h_new = o * F.tanh(c_new)      # output gateに通す
+
+        self.h, self.c = h_new, c_new
+        return h_new
+        
